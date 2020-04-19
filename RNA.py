caccd2a7402df5e2caeba5f96b19c1bba694e1e2
Anthony Agatiello <anthonya1999@gmail.com>
2020-04-18 20:05:57 -0400

    Add dictionary with count of number of IP addresses for each type of packet

--- RNA.py
+++ RNA.py
@@ -3,18 +3,22 @@
 # https://pypi.org/project/django-block-ip/
 import random
 import argparse
-import socket
+from socket import *
 from requests import get
 from contextlib import closing
 from scapy.all import *
 import smtplib
 from email.mime.text import MIMEText
+from struct import unpack
 
-def email(port_being_scanned):  # Email function
+def email(port_being_scanned, src_addr):  # Email function
     fromaddr = "rnanetworkdefense@outlook.com"
     password = "7h3r3'5 n0 p01n7 1n t4k1ng 7h15 p455w0rd fr0m m3... bu7 0k!"
     toaddrs  = "anthony@adatechri.com"
-    text_msg = "New scan detected on port %s" + str(port_being_scanned)
+    if port_being_scanned != 0:
+        text_msg = "New scan detected on port %s" + str(port_being_scanned)
+    else:
+        text_msg = "SYN flood detected from IP: " + str(src_addr)
     
     msg = MIMEText(text_msg)
     msg['Subject'] = "RNA Network Defense Tool Notification"
@@ -28,29 +32,85 @@ def email(port_being_scanned):  # Email function
     s.quit()
     
     return 0
+    
+def is_private_address(ip):
+    f = unpack('!I',inet_pton(AF_INET,ip))[0]
+    private = (
+        # 127.0.0.0
+        [2130706432, 4278190080],
+        # 192.168.0.0
+        [3232235520, 4294901760],
+        # 172.16.0.0
+        [2886729728, 4293918720],
+        # 10.0.0.0
+        [167772160,  4278190080],
+    )
+    for net in private:
+        if (f & net[1]) == net[0]:
+            return True
+    
+    return False
+
+def has_syn_flag(pkt):
+    if pkt[TCP].flags & 0x02:
+        return True
+    else:
+        return False
+
+# Make a hashtable (dictionary) with all the IPs the filter
+# has seen and keep track of how many times each IP appears
+#GOOD PRESENTING POINT FOR DR.OC
+knownTCP_IPs = {}
+knownUDP_IPs = {}
+knownICMP_IPs = {}
 
 def scan_filter(pkt): # Detects if a port scan is in progress
-    """Ip packet will be one of three types: TCP, UDP, or ICMP.
-    Todo."""
-    localIPAddr = socket.gethostbyname(socket.gethostname())  # Gets local ip address #127.0.0.1
-    tcpPorts = [port for port in range(1, 65536)]  # Initialize list with every tcp port
-    udpPorts = [port for port in range(1, 65536)]  # Initialize list with every udp port
+    # Ip packet will be one of three types: TCP, UDP, or ICMP.
     
-    if IP in pkt:  # If packet contains the IP header
+    # From this point forward, we have a packet we should look out for...
+    # If certain attributes of a packet are present, we should be
+    # able to detect malicious activity
+    
+    global knownTCP_IPs, knownUDP_IPs, knownICMP_IPs
+    localIPAddr = socket.gethostbyname(socket.gethostname())  # Gets local ip address 127.0.0.1
+    
+    if pkt.haslayer(IP) and pkt[IP].src == localIPAddr:  # If packet contains the IP header
         # If the source IP address of the packet is from the local machine, do not flag it.
-        if pkt[IP].src == localIPAddr:
-            return False
+        return False
+    # Most likely, you won't be getting suspicious traffic from within your local network
+    if pkt.haslayer(IP) and is_private_address(pkt[IP].src):
+        return False
     
-    # If a TCP packet's destination port is in the list (1-65535), flag it
-    if TCP in pkt and pkt[TCP].dport in tcpPorts:
+    # Declare source address once so we don't have to do it
+    # for every type of packet
+    source_ip_address = ""
+    if pkt.haslayer(IP):
+        source_ip_address = pkt[IP].src
+    
+    if pkt.haslayer(IP) and pkt.haslayer(TCP) and has_syn_flag(pkt):
+        if source_ip_address in knownTCP_IPs:
+            knownTCP_IPs[source_ip_address] += 1
+        else:
+            knownTCP_IPs.update( {source_ip_address : 1} )
+        
+        '''# TODO: Pass in S argument 1-10 w different level of sensitivity
+        if (knownTCP_IPs[source_ip_address] > x):'''
+        
         return True
     # If a UDP packet's destination port is in the list, flag it
-    elif UDP in pkt and pkt[UDP].dport in udpPorts:
+    elif pkt.haslayer(IP) and pkt.haslayer(UDP):
+        if source_ip_address in knownUDP_IPs:
+            knownUDP_IPs[source_ip_address] += 1
+        else:
+            knownUDP_IPs.update( {source_ip_address : 1} )
         return True
     # If an ICMP packet is being sent, flag it
-    elif ICMP in pkt:
+    elif pkt.haslayer(ICMP) in pkt:
+        if source_ip_address in knownICMP_IPs:
+            knownICMP_IPs[source_ip_address] += 1
+        else:
+            knownICMP_IPs.update( {source_ip_address : 1} )
         return True
-    # Nothing is happening, return false
     else:
         return False
 
@@ -71,7 +131,9 @@ def list_open_ports():
     return
     
 def print_packet_info(pkt):
-    return pkt.summary()  # Display packet summary information
+    # Display packet summary information
+    # return pkt.summary()
+    return None
     
 PARSER = argparse.ArgumentParser()  # Read in command line arguments
 
